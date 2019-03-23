9dd508bca3876883b48729d8a7ed8d12e8b3137b
mhabaj <mhabaj99@hotmail.com>
2019-03-23 04:43:50 +0100

    New Object Recognition engine
    Finished writing new ORE class .
    
    TO DO:
    -Figure out Traffic lights detection (colors ad lights)
    -Start looking into Sockets for server between PC/Raspberry
    -Find the most effecient Video streaming Format in LAN
    -Finish up the movment commands

--- CarProject/CarProject.cpp
+++ CarProject/CarProject.cpp
@@ -13,7 +13,7 @@ class ObjectScanner
 
 private:
 
-	const string CascadePanneauStop_XML="C:/Users/mhaba/OneDrive/Desktop/stopsign_classifier.xml";
+	const string CascadePanneauStop_XML="C:/Users/mhaba/OneDrive/Desktop/FeuxDeCirculation.xml";
 	const string CascadeFeuVert_XML="C:/Users/mhaba/OneDrive/Desktop/FeuxDeCirculation.xml";
 	const string CascadeFeuRouge_XML="C:/Users/mhaba/OneDrive/Desktop/FeuxDeCirculation.xml";
 	const string videoSourceURL = "http://192.168.0.26:8080/video/jpeg";
@@ -28,8 +28,8 @@ private:
 
 public:
 	ObjectScanner();
-	bool sceneScan();
-	bool returnFoundObjects(Mat frame, Mat Gray_Transform);
+	int sceneScan();  // 3 si Stop, 4 si Vert, 5 si Rouge
+	void returnFoundObjects(Mat frame, Mat Gray_Transform);
 	bool returnIsStop();
 
 };
@@ -38,7 +38,9 @@ ObjectScanner::ObjectScanner()
 {
 }
 
-bool ObjectScanner::sceneScan()
+
+
+int ObjectScanner::sceneScan()
 {
 
 	// ----Integrer les cascades:
@@ -63,7 +65,8 @@ bool ObjectScanner::sceneScan()
 	Mat frame;
 	Mat frame_resized;
 	Mat Gray_Transform;
-	while (this->VideoSource.read(frame)&& this->PanneauStopVec.size() <= 0)
+	while (this->VideoSource.read(frame)&& this->PanneauStopVec.size() <= 0
+		&& this->FeuRougeVec.size() <= 0 && this->FeuVertVec.size() <=0)
 	{
 		if (frame.empty())
 		{
@@ -73,55 +76,71 @@ bool ObjectScanner::sceneScan()
 
 		//---- On modifie la taille de l'image (pour les performances) et on applique les Classifiers 
 		resize(frame, frame_resized,Size(320, 220));
-		cout << this->PanneauStopVec.size()<<endl;
-		
-		
-			returnFoundObjects(frame_resized, Gray_Transform);
-		
-		
 
+		cout << this->PanneauStopVec.size()<<","<< this->FeuVertVec.size() << 
+			"," << this->FeuRougeVec.size() << endl;
+		
+		
+		returnFoundObjects(frame_resized, Gray_Transform);
+		
 		// Ctrl C pour quitter
 		if (waitKey(10) == 27)
 		{
 			break; 
 		}
-	}if(this->PanneauStopVec.size() > 0)
-		{
-			return true;
-		}
+	}
+	if(this->PanneauStopVec.size() > 0)
+	{
+		return 3;
+	}
+	if (this->FeuVertVec.size() > 0)
+	{
+		return 4;
+	}
+	if (this->FeuRougeVec.size() > 0)
+	{
+		return 5;
+	}
 	
 
 }
 
-bool ObjectScanner::returnFoundObjects(Mat frame, Mat Gray_Transformed)
+void ObjectScanner::returnFoundObjects(Mat frame, Mat Gray_Transformed)
 {
 		//LE SOURCE DE LAG : cvtColor FONCTION POUR TRANSFORMER LIMAGE EN NIVEAUX DE GRIS
 		cvtColor(frame, Gray_Transformed, COLOR_RGB2GRAY);
 		
-	// DETECT MULTISCALE POUR LES OBJETS 
+	// DETECT MULTISCALE POUR LES STOPS 
 	this->CascadePanneauStop.detectMultiScale(Gray_Transformed, this->PanneauStopVec);
-	this->isStop = false;
 
-	
 	for (size_t i = 0; i < this->PanneauStopVec.size(); i++)
 	{
 		//this->isStop = false;
 		rectangle(frame, this->PanneauStopVec[i], Scalar(0, 100, 0));
-		this->isStop = true;
 		
 	}
-if (this->PanneauStopVec.size() > 0) {
-			return true;
-		}
+
+	// DETECT MULTISCALE POUR LES FEUX VERTS 
+	this->CascadePanneauStop.detectMultiScale(Gray_Transformed, this->FeuVertVec);
+
+	for (size_t i = 0; i < this->FeuVertVec.size(); i++)
+	{
+		rectangle(frame, this->FeuVertVec[i], Scalar(0, 100, 0));
+
+	}
+
+
+	// DETECT MULTISCALE POUR LES FEUX ROUGES 
+	this->CascadePanneauStop.detectMultiScale(Gray_Transformed, this->FeuRougeVec);
+
+	for (size_t i = 0; i < this->FeuRougeVec.size(); i++)
+	{
+		rectangle(frame, this->FeuRougeVec[i], Scalar(0, 100, 0));
+
+	}
+
 	imshow("", frame);
-	
-	
-
-	
-	
-
-	
-	
+		
 }
 
 bool ObjectScanner::returnIsStop()
@@ -136,9 +155,17 @@ void main()
 	ObjectScanner obj1;
 	
 
-	if (obj1.sceneScan()) {
+	if (obj1.sceneScan()==3) {
 
 		cout << "ATTNETION STOP" << endl;
+	}
+	if (obj1.sceneScan() == 4) {
+
+		cout << "FEUX VERT" << endl;
+	}
+	if (obj1.sceneScan() == 3) {
+
+		cout << "FEUX ROUGE" << endl;
 	}
 	
 }
